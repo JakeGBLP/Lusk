b1b1240681f900c563f331b649c3e0445fe99185
Jake <gabrieleluparelloj@gmail.com>
2023-04-19 13:47:17 +0200

    Added Horse Related Elements

---
+++ src/main/java/me/jake/lusk/elements/expressions/ExprHorseJumpStrength.java
@@ -0,0 +1,77 @@
+package me.jake.lusk.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.classes.Changer;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import ch.njol.util.coll.CollectionUtils;
+import org.bukkit.entity.AbstractHorse;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Horse - Jump Strength")
+@Description("Returns the jump strength of an horse.\nCan be set.")
+@Examples({"broadcast jumping strength of target"})
+@Since("1.0.3")
+public class ExprHorseJumpStrength extends SimpleExpression<Double> {
+    static {
+        Skript.registerExpression(ExprHorseJumpStrength.class, Double.class, ExpressionType.COMBINED,
+                "[the] [horse] jump[ing] (strength|force) of %entity%");
+    }
+
+    private Expression<Entity> entityExpression;
+
+    @SuppressWarnings("unchecked")
+    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull SkriptParser.ParseResult parseResult) {
+        entityExpression = (Expression<Entity>) exprs[0];
+        return true;
+    }
+    @Override
+    protected Double @NotNull [] get(@NotNull Event e) {
+        Entity entity = entityExpression.getSingle(e);
+        if (entity instanceof AbstractHorse horse) {
+            return new Double[]{horse.getJumpStrength()};
+        }
+        return new Double[0];
+    }
+    @Override
+    public Class<?> @NotNull [] acceptChange(Changer.@NotNull ChangeMode mode) {
+        if (mode == Changer.ChangeMode.SET) {
+            return CollectionUtils.array(Double[].class);
+        }
+        return new Class[0];
+    }
+    @Override
+    public void change(@NotNull Event e, Object @NotNull [] delta, Changer.@NotNull ChangeMode mode) {
+        Double aDouble = delta instanceof Double[] ? ((Double[]) delta)[0] : null;
+        if (aDouble == null) return;
+        Entity entity = entityExpression.getSingle(e);
+        if (entity instanceof AbstractHorse horse) {
+            horse.setJumpStrength(aDouble);
+        }
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public @NotNull Class<? extends Double> getReturnType() {
+        return Double.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "the horse jumping strength of " + (e == null ? "" : entityExpression.getSingle(e));
+    }
+}
