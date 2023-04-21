87a365725527197220224a8e3fa39e99f4c22488
Jake <gabrieleluparelloj@gmail.com>
2023-04-21 14:48:00 +0200

    Added Command Block Elements and 1.20 Sign Elements.

---
+++ src/main/java/me/jake/lusk/elements/expressions/ExprSignEditableState.java
@@ -0,0 +1,77 @@
+package me.jake.lusk.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.classes.Changer;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import ch.njol.util.coll.CollectionUtils;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Name("Sign - Editable State")
+@Description("Returns whether or not a sign is editable.\nCan be set.")
+@Examples({"broadcast editable state of target"})
+@Since("1.0.3")
+public class ExprSignEditableState extends SimpleExpression<Boolean> {
+    static {
+        Skript.registerExpression(ExprSignEditableState.class, Boolean.class, ExpressionType.COMBINED,
+                "[the] [sign] editable state of %block%");
+    }
+
+    private Expression<Block> blockExpression;
+
+    @SuppressWarnings("unchecked")
+    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull SkriptParser.ParseResult parseResult) {
+        blockExpression = (Expression<Block>) exprs[0];
+        return true;
+    }
+    @Override
+    protected Boolean @NotNull [] get(@NotNull Event e) {
+        Block block = blockExpression.getSingle(e);
+        if (block.getState() instanceof Sign sign) {
+            return new Boolean[]{sign.isEditable()};
+        }
+        return new Boolean[0];
+    }
+    @Override
+    public Class<?> @NotNull [] acceptChange(Changer.@NotNull ChangeMode mode) {
+        if (mode == Changer.ChangeMode.SET) {
+            return CollectionUtils.array(Boolean[].class);
+        }
+        return new Class[0];
+    }
+    @Override
+    public void change(@NotNull Event e, Object @NotNull [] delta, Changer.@NotNull ChangeMode mode) {
+        Boolean aBoolean = delta instanceof Boolean[] ? ((Boolean[]) delta)[0] : null;
+        if (aBoolean == null) return;
+        Block block = blockExpression.getSingle(e);
+        if (block.getState() instanceof Sign sign) {
+            sign.setEditable(aBoolean);
+        }
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public @NotNull Class<? extends Boolean> getReturnType() {
+        return Boolean.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "the sign editable state of " + (e == null ? "" : blockExpression.getSingle(e));
+    }
+}
