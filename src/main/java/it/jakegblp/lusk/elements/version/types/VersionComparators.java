72a534edfd8be6d075ea5ece759227b7cee15678
Jake <gabrieleluparelloj@gmail.com>
2024-07-15 21:25:39 +0200

    Renamed Comparators.java to VersionComparators.java for more clarity
    Took 39 seconds

--- src/main/java/it/jakegblp/lusk/elements/version/types/VersionComparators.java
+++ src/main/java/it/jakegblp/lusk/elements/version/types/VersionComparators.java
@@ -3,12 +3,13 @@ package it.jakegblp.lusk.elements.version.types;
 import com.vdurmont.semver4j.Semver;
 import org.jetbrains.annotations.NotNull;
 import org.skriptlang.skript.lang.comparator.Comparator;
+import org.skriptlang.skript.lang.comparator.Comparators;
 import org.skriptlang.skript.lang.comparator.Relation;
 
-public class Comparators {
+public class VersionComparators {
 
     static {
-        org.skriptlang.skript.lang.comparator.Comparators.registerComparator(Semver.class, Semver.class, new Comparator<>() {
+        Comparators.registerComparator(Semver.class, Semver.class, new Comparator<>() {
             @Override
             public @NotNull Relation compare(Semver v1, Semver v2) {
                 return v1.isGreaterThan(v2) ? Relation.GREATER : v2.isGreaterThan(v1) ? Relation.SMALLER : Relation.EQUAL;
@@ -21,6 +22,6 @@ public class Comparators {
         });
     }
 
-    public Comparators() {
+    public VersionComparators() {
     }
 }
