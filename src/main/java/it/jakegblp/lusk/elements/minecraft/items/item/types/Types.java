463b5d24d92918b623b6f67505c9055e761ce9fc
Jake <gabrieleluparelloj@gmail.com>
2024-07-15 21:15:12 +0200

    Item Rarity classinfo
    Took 44 seconds

---
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/items/item/types/Types.java
@@ -0,0 +1,27 @@
+package it.jakegblp.lusk.elements.minecraft.items.item.types;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.registrations.Classes;
+import it.jakegblp.lusk.wrappers.EnumWrapper;
+import org.bukkit.inventory.ItemRarity;
+
+public class Types {
+    static {
+        if (Skript.classExists("org.bukkit.inventory.ItemRarity") && Classes.getExactClassInfo(ItemRarity.class) == null) {
+            EnumWrapper<ItemRarity> ITEMRARITY_ENUM = new EnumWrapper<>(ItemRarity.class);
+            Classes.registerClass(ITEMRARITY_ENUM.getClassInfo("itemrarity")
+                    .user("item ?rarit(y|ies)")
+                    .name("Item Rarity")
+                    .description("""
+                                The Rarity of an item.
+                                
+                                COMMON = White
+                                UNCOMMON = Yellow
+                                RARE = Aqua
+                                EPIC = Light Purple
+                                """)
+                    .examples("item rarity of dragon egg")
+                    .since("1.2"));
+        }
+    }
+}
