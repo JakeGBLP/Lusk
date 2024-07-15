f48e978e76bd4fddd2e2231d580cdffddc9fd9f8
Jake <gabrieleluparelloj@gmail.com>
2024-07-15 21:13:09 +0200

    Updated Item Rarity stuff
    Took 1 minute

--- src/main/java/it/jakegblp/lusk/elements/minecraft/items/item/expressions/ExprRarity.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/items/item/expressions/ExprRarity.java
@@ -2,6 +2,7 @@ package it.jakegblp.lusk.elements.minecraft.items.item.expressions;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.ItemType;
+import ch.njol.skript.classes.Changer;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
@@ -11,45 +12,44 @@ import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.Event;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ItemRarity;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-@Name("Item/Enchantment Rarity")
-@Description("Returns the rarity of an item/enchantment")
-@Examples({"broadcast rarity of tool\nbroadcast rarity of mending"})
-@Since("1.0.0")
-public class ExprRarity extends SimpleExpression<String> {
+@Name("Item - Rarity")
+@Description("Returns the rarity of an item.\nCan be set.\n\nBefore Lusk 1.2 (and Minecraft 1.20.5), this expression returned strings and also worked for enchantments, due to some major changes enchantments no longer have a rarity.")
+@Examples({"broadcast item rarity of tool","set item rarity of {_sword} to epic"})
+@Since("1.0.0+, 1.2+ (ItemRarity)")
+public class ExprRarity extends SimpleExpression<ItemRarity> {
     static {
-        Skript.registerExpression(ExprRarity.class, String.class, ExpressionType.SIMPLE,
-                "rarity of %enchantment/itemtype%",
-                "%enchantment/itemtype%'[s] rarity");
+        if (Skript.classExists("org.bukkit.inventory.ItemRarity")) {
+            Skript.registerExpression(ExprRarity.class, ItemRarity.class, ExpressionType.COMBINED,
+                    "item rarity of %itemtype%",
+                    "%itemtype%'[s] item rarity");
+        }
     }
 
-    private Expression<Object> object;
+    private Expression<ItemType> item;
 
     @SuppressWarnings("unchecked")
     public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull ParseResult parseResult) {
-        object = (Expression<Object>) exprs[0];
+        item = (Expression<ItemType>) exprs[0];
         return true;
     }
 
     @Override
-    protected String @NotNull [] get(@NotNull Event e) {
-        Object o = object.getSingle(e);
-        String string;
-        if (o instanceof ItemType) {
-            string = new ItemStack(((ItemType) o).getMaterial()).getRarity().toString();
-        } else if (o instanceof Enchantment) {
-            string = ((Enchantment) o).getRarity().toString();
-        } else {
-            return new String[]{};
+    protected ItemRarity @NotNull [] get(@NotNull Event e) {
+        ItemType itemType = item.getSingle(e);
+        if (itemType != null) {
+            ItemMeta meta = itemType.getItemMeta();
+            if (meta.hasRarity()) {
+                return new ItemRarity[]{meta.getRarity()};
+            }
         }
-        return new String[]{string};
+        return new ItemRarity[0];
     }
-
 
     @Override
     public boolean isSingle() {
@@ -57,12 +57,28 @@ public class ExprRarity extends SimpleExpression<String> {
     }
 
     @Override
-    public @NotNull Class<? extends String> getReturnType() {
-        return String.class;
+    public @NotNull Class<? extends ItemRarity> getReturnType() {
+        return ItemRarity.class;
+    }
+
+    @Override
+    public Class<?>[] acceptChange(Changer.@NotNull ChangeMode mode) {
+        return mode == Changer.ChangeMode.SET ? new Class[]{ItemRarity.class} : null;
+    }
+
+    @Override
+    public void change(@NotNull Event e, Object @NotNull [] delta, Changer.@NotNull ChangeMode mode) {
+        if (delta[0] instanceof ItemRarity itemRarity) {
+            ItemType itemType = item.getSingle(e);
+            if (itemType != null) {
+                ItemMeta meta = itemType.getItemMeta();
+                meta.setRarity(itemRarity);
+            }
+        }
     }
 
     @Override
     public @NotNull String toString(@Nullable Event e, boolean debug) {
-        return "rarity of " + (e == null ? "" : object.toString(e, debug));
+        return "item rarity of " + (e == null ? "" : item.toString(e, debug));
     }
 }
