763b8e4ad1d5ef43b563f9d683d30c4e4a357866
Jake <gabrieleluparelloj@gmail.com>
2024-08-07 02:43:38 +0200

    trusted players expr

---
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/entities/neutral/fox/expressions/ExprFoxTrustedPlayers.java
@@ -0,0 +1,139 @@
+package it.jakegblp.lusk.elements.minecraft.entities.neutral.fox.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.classes.Changer;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.PropertyExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Fox;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Name("Fox - Trusted Players")
+@Description("""
+Returns the first, second, or both trusted players of 1 or more foxes.
+First and second can both be set to a single offline player, trusted players can be set to 2 offline players (or 1, which only sets the first one).
+
+Note: Before the second trusted player of a fox can be set, the first one must also be set, in the same way, you can't clear the first one if the second one is set, trying to do either of those things won't do anything
+
+If you wish to set/clear both, do so by using the second pattern: `clear trusted players of {_fox}`
+""")
+@Examples({"broadcast first trusted player of {_fox}","set trusted players of {_fox} to {_notch} and {_steve}"})
+@Since("1.2")
+public class ExprFoxTrustedPlayers extends PropertyExpression<LivingEntity, OfflinePlayer> {
+
+    static {
+        Skript.registerExpression(ExprFoxTrustedPlayers.class, OfflinePlayer.class, ExpressionType.PROPERTY,
+                "[the] [first|:second] trusted player of %livingentities%",
+                "%livingentities%'[s] [first|:second] trusted player",
+                "[the] trusted players of %livingentities%",
+                "%livingentities%'[s] trusted players");
+    }
+
+    int state;
+
+    @Override
+    protected OfflinePlayer @NotNull [] get(@NotNull Event event, LivingEntity @NotNull [] source) {
+        List<OfflinePlayer> trusted = new ArrayList<>();
+        for (LivingEntity livingEntity : source) {
+            if (livingEntity instanceof Fox fox) {
+                if (state != 1 && fox.getFirstTrustedPlayer() instanceof OfflinePlayer player)
+                    trusted.add(player);
+                if (state != 0 && fox.getSecondTrustedPlayer() instanceof OfflinePlayer player)
+                    trusted.add(player);
+            }
+        }
+        return trusted.toArray(new OfflinePlayer[0]);
+    }
+
+    @Override
+    public boolean isSingle() {
+        return state < 2;
+    }
+
+    @Override
+    public @NotNull Class<? extends OfflinePlayer> getReturnType() {
+        return OfflinePlayer.class;
+    }
+
+    @Override
+    public @Nullable Class<?>[] acceptChange(Changer.@NotNull ChangeMode mode) {
+        if (mode == Changer.ChangeMode.DELETE || mode == Changer.ChangeMode.RESET) return new Class[0];
+        else if (mode == Changer.ChangeMode.SET) {
+            return new Class[]{state < 2 ? OfflinePlayer.class : OfflinePlayer[].class};
+        }
+        return null;
+    }
+
+    @Override
+    public void change(@NotNull Event event, @Nullable Object[] delta, Changer.@NotNull ChangeMode mode) {
+        if (mode == Changer.ChangeMode.DELETE || mode == Changer.ChangeMode.RESET) {
+            if (state == 0) {
+                for (LivingEntity entity : getExpr().getArray(event)) {
+                    if (entity instanceof Fox fox && fox.getSecondTrustedPlayer() == null) fox.setFirstTrustedPlayer(null);
+                }
+            } else if (state == 1) {
+                for (LivingEntity entity : getExpr().getArray(event)) {
+                    if (entity instanceof Fox fox) fox.setSecondTrustedPlayer(null);
+                }
+            } else {
+                for (LivingEntity entity : getExpr().getArray(event)) {
+                    if (entity instanceof Fox fox) {
+                        fox.setSecondTrustedPlayer(null);
+                        fox.setFirstTrustedPlayer(null);
+                    }
+                }
+            }
+        } else if (mode == Changer.ChangeMode.SET) {
+            if (delta == null || delta[0] == null) return;
+            if (state == 2 && delta instanceof OfflinePlayer[] offlinePlayers) {
+                for (LivingEntity entity : getExpr().getArray(event)) {
+                    if (entity instanceof Fox fox) {
+                        if (offlinePlayers[0] != null) fox.setFirstTrustedPlayer(offlinePlayers[0]);
+                        if (offlinePlayers[1] != null) fox.setFirstTrustedPlayer(offlinePlayers[1]);
+                    }
+                }
+            } else if (delta[0] instanceof OfflinePlayer offlinePlayer) {
+                if (state == 0) {
+                    for (LivingEntity entity : getExpr().getArray(event)) {
+                        if (entity instanceof Fox fox) fox.setFirstTrustedPlayer(offlinePlayer);
+                    }
+                } else if (state == 1) {
+                    for (LivingEntity entity : getExpr().getArray(event)) {
+                        if (entity instanceof Fox fox && fox.getFirstTrustedPlayer() != null) fox.setSecondTrustedPlayer(offlinePlayer);
+                    }
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event event, boolean debug) {
+        StringBuilder builder = new StringBuilder();
+        if (state == 2) builder.append("trusted players");
+        else builder.append(state == 0 ? "first" : "second").append(" player");
+        builder.append(" of ").append(event != null ? getExpr().toString(event,debug) : "");
+        return builder.toString();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?> @NotNull [] expressions, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        state = matchedPattern > 1 ? 2 : (parseResult.hasTag("second") ? 1 : 0);
+        setExpr((Expression<? extends LivingEntity>) expressions[0]);
+        return true;
+    }
+}
