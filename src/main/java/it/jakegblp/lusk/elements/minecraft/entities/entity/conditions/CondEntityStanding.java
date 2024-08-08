ac052dd4bdc086c258fd684148b50c8bc19d587a
Jake <gabrieleluparelloj@gmail.com>
2024-08-08 09:11:17 +0200

    final elements

---
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/entities/entity/conditions/CondEntityStanding.java
@@ -0,0 +1,32 @@
+package it.jakegblp.lusk.elements.minecraft.entities.entity.conditions;
+
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import org.bukkit.entity.*;
+import org.jetbrains.annotations.NotNull;
+
+@Name("Entity - is Standing")
+@Description("Checks if an entity is standing, mainly polar bears, but other entities can be standing.")
+@Examples({"on damage of polar bear:\n\tif victim is standing:\n\t\tcancel event\n\t\tsend \"you can't, %victim% is standing on business\" to attacker"})
+@Since("1.2")
+public class CondEntityStanding extends PropertyCondition<LivingEntity> {
+    static {
+        register(CondEntityStanding.class, "standing", "livingentities");
+    }
+
+    @Override
+    public boolean check(LivingEntity entity) {
+        if (entity instanceof PolarBear polarBear) {
+            return polarBear.isStanding();
+        }
+        return entity.getPose().equals(Pose.STANDING);
+    }
+
+    @Override
+    protected @NotNull String getPropertyName() {
+        return "standing";
+    }
+}
---
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/entities/hostile/vindicator/conditions/CondVindicatorJohnny.java
@@ -0,0 +1,30 @@
+package it.jakegblp.lusk.elements.minecraft.entities.hostile.vindicator.conditions;
+
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Vindicator;
+import org.jetbrains.annotations.NotNull;
+
+@Name("Vindicator - is Johnny")
+@Description("Checks if a vindicator is Johnny.")
+@Examples({"if {_vindicator} is johnny:"})
+@Since("1.2")
+public class CondVindicatorJohnny extends PropertyCondition<LivingEntity> {
+    static {
+        register(CondVindicatorJohnny.class, "johnny", "livingentities");
+    }
+
+    @Override
+    public boolean check(LivingEntity entity) {
+        return entity instanceof Vindicator vindicator && vindicator.isJohnny();
+    }
+
+    @Override
+    protected @NotNull String getPropertyName() {
+        return "johnny";
+    }
+}
