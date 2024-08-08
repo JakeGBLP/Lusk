7f28a4b151c3236d91af73377e9eb6f3d25e6406
Jake <gabrieleluparelloj@gmail.com>
2024-08-08 09:05:02 +0200

    rollback for main branch

--- src/main/java/it/jakegblp/lusk/elements/text/expressions/ExprCases.java
+++ src/main/java/it/jakegblp/lusk/elements/text/expressions/ExprCases.java
@@ -10,41 +10,22 @@ import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
-import it.jakegblp.lusk.utils.TextUtils;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import static it.jakegblp.lusk.utils.TextUtils.toFont;
-
 @Name("Small Capital Case")
-@Description("Returns the given string with the small font.\n'Strict' allows uppercase SMALL to be included.")
+@Description("Returns the given string with the small font.\n'Strict' allows uppercase characters to be included.")
 @Examples({"broadcast player's name in small caps"})
 @Since("1.0.0")
-public class ExprCases extends SimpleExpression<String> {                                                                                                               //
-    /*
-    https://en.wikipedia.org/wiki/Modifier_letter_left_half_ring
-    https://rupertshepherd.info/resource_pages/superscript-letters-in-unicode
-    https://lingojam.com/SuperscriptGenerator
-    > https://en.wikipedia.org/wiki/Unicode_subscripts_and_superscripts
-    https://www.compart.com/en/unicode/block/U+2070
-
-
-     */
-
-                                                                                                                                                                        //abcdefghijklmnopqrstuvwxyz1234567890_!#%)(+-/\<>:;"'[]?
-    public static Object[] NORMAL                =    {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','_','!','#','%','(',')','+','-','=','/','<','>',':',';','"','\'','[',']','?'};
-    public static Object[] SMALL                 =    {'ᴀ','ʙ','ᴄ','ᴅ','ᴇ','ғ','ɢ','ʜ','ɪ','ᴊ','ᴋ','ʟ','ᴍ','ɴ','ᴏ','ᴘ','ǫ','ʀ','s','ᴛ','ᴜ','ᴠ','ᴡ','x','ʏ','ᴢ','₁','₂','₃','₄','₅','₆','₇','₈','₉','₀','ˍ',' ',' ','⁒','₍','₎' ,'₊','₋','₌'};
-    public static Object[] SUBSCRIPT_MINUSCULE   =    {'ᵃ','ᵇ','ᶜ','ᵈ','ᵉ','ᶠ' ,'ᵍ','ʰ','ᶦ','ʲ','ᵏ','ˡ','ᵐ','ⁿ' ,'ᵒ','ᵖ',"𐞥",'ʳ','ˢ','ᵗ','ᵘ','ᵛ','ʷ','ˣ','ʸ','ᶻ'};
-    public static Object[] SUPERSCRIPT_CAPITAL   =    {'ᴬ','ᴮ','ʿ','ᴰ','ᴱ','ꟳ','ᴳ','ᴴ','ᴵ','ᴶ','ᴷ','ᴸ','ᴹ','ᴺ','ᴼ','ᴾ','ꟴ','ᴿ','꟱','ᵀ','ᵁ','ⱽ','ᵂ'};
-    public static Object[] SUPERSCRIPT_MINUSCULE =    {'ₐ',             'ₑ',        'ₕ','ᵢ','ⱼ' ,'ₖ','ₗ','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹','⁰','⁽','⁾','⁺','⁻','⁼'};
-    public static Object[] TINY                  =    {'ᶻ','ˍ','﹗','﹟'};
+public class ExprCases extends SimpleExpression<String> {
+    public static Character[] characters = {'ᴀ', 'ʙ', 'ᴄ', 'ᴅ', 'ᴇ', 'ғ', 'ɢ', 'ʜ', 'ɪ', 'ᴊ', 'ᴋ', 'ʟ', 'ᴍ', 'ɴ', 'ᴏ', 'ᴘ', 'ǫ', 'ʀ', 's', 'ᴛ', 'ᴜ', 'ᴠ', 'ᴡ', 'x', 'ʏ', 'ᴢ'};
 
     public static String toSmallFont(String string, boolean strict) {
         String regex = strict ? "[a-zA-Z]" : "[a-z]";
         for (String letter : string.split("")) {
             if (letter.matches(regex)) {
-                string = string.replaceAll(letter, String.valueOf(SMALL[(Character.getNumericValue(letter.charAt(0)) - 10)]));
+                string = string.replaceAll(letter, String.valueOf(characters[(Character.getNumericValue(letter.charAt(0)) - 10)]));
             }
         }
         return string;
@@ -55,18 +36,7 @@ public class ExprCases extends SimpleExpression<String> {                       
                 "%string% in [strict:(strict|fully)] small (font|[upper[ ]]case|cap(s|ital[ case]))",
                 "[strict:(strict|fully)] small (font|[upper[ ]]case|cap(s|ital[ case])) %string%");
     }
-    "%strings% in [strict|:lenient] small (caps|capital case)",
-    "%strings% in [:upper|:lower|:title] super[ ]script case",
-    "%strings% in [strict|:lenient] sub[ ]script case",
 
-
-    DEFAULT(-0),
-    SMALL_CAPITAL(0),
-    SUPERSCRIPT_CAPITAL(1),
-    SUPERSCRIPT_MINUSCULE(2),
-    SUBSCRIPT_MINUSCULE(3),
-    TINY(4),
-    SUPERSCRIPT(5);
     private Expression<String> string;
 
     private boolean strict;
@@ -81,7 +51,7 @@ public class ExprCases extends SimpleExpression<String> {                       
     @Override
     protected String @NotNull [] get(@NotNull Event e) {
         String s = string.getSingle(e);
-        if (s != null) return new String[]{toFont(TextUtils.LuskFont.SMALL_CAPITAL, s,false)};
+        if (s != null) return new String[]{toSmallFont(s, strict)};
         return new String[0];
     }
 
