5b078c131c4a16b09a380380cc14c8c154e81117
Jake <gabrieleluparelloj@gmail.com>
2024-11-18 06:00:01 +0100

    Switched ExprRarity to simple property expression

--- src/main/java/it/jakegblp/lusk/elements/minecraft/item/expressions/ExprRarity.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/item/expressions/ExprRarity.java
@@ -1,21 +1,15 @@
 package it.jakegblp.lusk.elements.minecraft.item.expressions;
 
-import ch.njol.skript.Skript;
-import ch.njol.skript.aliases.ItemType;
 import ch.njol.skript.classes.Changer;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.ExpressionType;
-import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.skript.lang.util.SimpleExpression;
-import ch.njol.util.Kleenean;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemRarity;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import static it.jakegblp.lusk.utils.Constants.HAS_SPIGOT_ITEM_RARITY;
@@ -25,64 +19,39 @@ import static it.jakegblp.lusk.utils.Constants.HAS_SPIGOT_ITEM_RARITY;
 @Examples({"broadcast item rarity of tool", "set item rarity of {_sword} to epic"})
 @Since("1.0.0+, 1.2+ (ItemRarity)")
 @SuppressWarnings("unused")
-public class ExprRarity extends SimpleExpression<ItemRarity> {
+public class ExprRarity extends SimplePropertyExpression<ItemStack,ItemRarity> {
+
     static {
-        // TODO: PROPERTY EXPR
         if (HAS_SPIGOT_ITEM_RARITY) {
-            Skript.registerExpression(ExprRarity.class, ItemRarity.class, ExpressionType.PROPERTY,
-                    "item rarity of %itemtype%",
-                    "%itemtype%'[s] item rarity");
+            register(ExprRarity.class, ItemRarity.class, "item rarity", "itemstacks");
         }
     }
 
-    private Expression<ItemType> item;
-
-    @SuppressWarnings("unchecked")
-    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull ParseResult parseResult) {
-        item = (Expression<ItemType>) exprs[0];
-        return true;
+    @Override
+    public @Nullable ItemRarity convert(ItemStack from) {
+        ItemMeta meta = from.getItemMeta();
+        return meta.hasRarity() ? meta.getRarity() : null;
     }
 
     @Override
-    protected ItemRarity @NotNull [] get(@NotNull Event e) {
-        ItemType itemType = item.getSingle(e);
-        if (itemType != null) {
-            ItemMeta meta = itemType.getItemMeta();
-            if (meta.hasRarity()) {
-                return new ItemRarity[]{meta.getRarity()};
-            }
-        }
-        return new ItemRarity[0];
+    protected String getPropertyName() {
+        return "item rarity";
     }
 
     @Override
-    public boolean isSingle() {
-        return true;
-    }
-
-    @Override
-    public @NotNull Class<? extends ItemRarity> getReturnType() {
+    public Class<? extends ItemRarity> getReturnType() {
         return ItemRarity.class;
     }
 
     @Override
-    public Class<?>[] acceptChange(Changer.@NotNull ChangeMode mode) {
-        return mode == Changer.ChangeMode.SET ? new Class[]{ItemRarity.class} : null;
+    public @Nullable Class<?>[] acceptChange(Changer.ChangeMode mode) {
+        return mode == Changer.ChangeMode.SET ? new Class[] {ItemRarity.class} : null;
     }
 
     @Override
-    public void change(@NotNull Event e, Object @NotNull [] delta, Changer.@NotNull ChangeMode mode) {
-        ItemType itemType = item.getSingle(e);
-        if (itemType != null) {
-            if (delta[0] instanceof ItemRarity itemRarity) {
-                ItemMeta meta = itemType.getItemMeta();
-                meta.setRarity(itemRarity);
-            }
+    public void change(Event event, @Nullable Object[] delta, Changer.ChangeMode mode) {
+        if (mode == Changer.ChangeMode.SET && delta != null && delta[0] instanceof ItemRarity itemRarity) {
+            getExpr().stream(event).forEach(itemStack -> itemStack.getItemMeta().setRarity(itemRarity));
         }
-    }
-
-    @Override
-    public @NotNull String toString(@Nullable Event e, boolean debug) {
-        return "item rarity of " + (e == null ? "" : item.toString(e, debug));
     }
 }
