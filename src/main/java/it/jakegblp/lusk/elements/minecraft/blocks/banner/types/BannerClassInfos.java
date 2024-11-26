9f43eec2e17a832b36015144cc8d6e2138187c09
Jake <gabrieleluparelloj@gmail.com>
2024-11-26 23:59:34 +0100

    Added parser to Banner Pattern Object.

--- src/main/java/it/jakegblp/lusk/elements/minecraft/blocks/banner/types/BannerClassInfos.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/blocks/banner/types/BannerClassInfos.java
@@ -2,10 +2,13 @@ package it.jakegblp.lusk.elements.minecraft.blocks.banner.types;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.classes.Parser;
+import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
 import it.jakegblp.lusk.api.wrappers.EnumRegistryWrapper;
 import org.bukkit.block.banner.Pattern;
 import org.bukkit.block.banner.PatternType;
+import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("unused")
 public class BannerClassInfos {
@@ -22,10 +25,31 @@ public class BannerClassInfos {
         }
         if (Skript.classExists("org.bukkit.block.banner.Pattern") && Classes.getExactClassInfo(Pattern.class) == null) {
             Classes.registerClass(new ClassInfo<>(Pattern.class, "bannerpattern")
-                        .user("banner patterns?")
+                        .user("banner ?patterns?")
                         .name("Banner Pattern")
                         .description("A Banner Pattern.") // add example
-                        .since("1.3"));
+                        .since("1.3")
+                    .parser(new Parser<>() {
+                        @Override
+                        public String toString(Pattern o, int flags) {
+                            return "banner pattern with pattern type "+o.getPattern()+" and color "+o.getColor();
+                        }
+
+                        @Override
+                        public String toVariableNameString(Pattern o) {
+                            return toString(o,0) + " banner pattern (" + o + ")";
+                        }
+
+                        @Override
+                        public @Nullable Pattern parse(String s, ParseContext context) {
+                            return null; // todo: might add at some point
+                        }
+
+                        @Override
+                        public boolean canParse(ParseContext context) {
+                            return false;
+                        }
+                    }));
         }
     }
 }
