22c10187ecfdccdf0c4ee2cecc037ea998d6d24c
Jake <gabrieleluparelloj@gmail.com>
2024-11-26 04:39:12 +0100

    Presumably the final fix to these tests and these expressions

--- src/main/java/it/jakegblp/lusk/elements/minecraft/entities/armorstand/expressions/ExprArmorStandHasArms.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/entities/armorstand/expressions/ExprArmorStandHasArms.java
@@ -16,7 +16,8 @@ import org.jetbrains.annotations.Nullable;
 import static it.jakegblp.lusk.utils.LuskUtils.registerVerbosePropertyExpression;
 
 @Name("Armor Stand - has Arms (Property)")
-@Description("""
+@Description(
+"""
 Gets and sets the `hasHarms` property of an armorstand entity or item, to do so with an armorstand item you must have Paper.
 """)
 @Examples({"set has arms property of target to true", "set whether armor stand target has arms to true"})
@@ -37,6 +38,7 @@ public class ExprArmorStandHasArms extends SimplePropertyExpression<Object, Bool
         return true;
     }
 
+
     @Override
     public @Nullable Class<?>[] acceptChange(Changer.ChangeMode mode) {
         return mode == Changer.ChangeMode.SET ? new Class[] {Boolean.class} : null;
@@ -45,7 +47,8 @@ public class ExprArmorStandHasArms extends SimplePropertyExpression<Object, Bool
     @Override
     public void change(Event event, @Nullable Object[] delta, Changer.ChangeMode mode) {
         if (mode == Changer.ChangeMode.SET && delta != null && delta[0] instanceof Boolean bool) {
-            getExpr().stream(event).forEach(object -> ArmorStandUtils.setHasArms(object, invisible ^ bool));
+            boolean finalBoolean = invisible ^ bool;
+            getExpr().stream(event).forEach(object -> ArmorStandUtils.setHasArms(object, finalBoolean));
         }
     }
 
