3e48a0fece803bf79317056e6d418d99fc9c79ac
Jake <gabrieleluparelloj@gmail.com>
2024-11-27 04:02:41 +0100

    Changed ExprBrewingFuel into a Simple Property Expression, slight documentation change.

--- src/main/java/it/jakegblp/lusk/elements/minecraft/blocks/brewingstand/expressions/ExprBrewingFuel.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/blocks/brewingstand/expressions/ExprBrewingFuel.java
@@ -1,82 +1,70 @@
 package it.jakegblp.lusk.elements.minecraft.blocks.brewingstand.expressions;
 
-import ch.njol.skript.Skript;
-import ch.njol.skript.aliases.ItemType;
 import ch.njol.skript.classes.Changer;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.ExpressionType;
-import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.skript.lang.util.SimpleExpression;
-import ch.njol.util.Kleenean;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import org.bukkit.block.Block;
 import org.bukkit.block.BrewingStand;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.BrewerInventory;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
-@Name("Brewing - Fuel")
+@Name("Brewing - Fuel Item")
 @Description("Returns the brewing fuel item of a Brewing Stand.\nCan be set.")
 @Examples({"on brewing start:\n\tbroadcast the brewing fuel of event-block"})
 @Since("1.0.2")
-public class ExprBrewingFuel extends SimpleExpression<ItemType> {
+@SuppressWarnings("unused")
+public class ExprBrewingFuel extends SimplePropertyExpression<Block, ItemStack> {
+
+    //todo: add tests
     static {
-        Skript.registerExpression(ExprBrewingFuel.class, ItemType.class, ExpressionType.COMBINED,
-                "[the] brewing fuel of %block%", "%block%'[s] brewing fuel");
-    }
-
-    private Expression<Block> blockExpression;
-
-    @SuppressWarnings("unchecked")
-    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull ParseResult parseResult) {
-        blockExpression = (Expression<Block>) exprs[0];
-        return true;
+        register(ExprBrewingFuel.class, ItemStack.class, "brewing fuel", "blocks");
     }
 
     @Override
-    protected ItemType @NotNull [] get(@NotNull Event e) {
-        Block block = blockExpression.getSingle(e);
-        if (block != null && block.getState() instanceof BrewingStand brewingStand) {
+    public @Nullable ItemStack convert(Block from) {
+        if (from.getState() instanceof BrewingStand brewingStand) {
             BrewerInventory brewerInventory = brewingStand.getInventory();
             if (brewerInventory.getFuel() != null) {
-                return new ItemType[]{new ItemType(brewerInventory.getFuel())};
+                return brewerInventory.getFuel();
             }
         }
-        return new ItemType[0];
+        return null;
     }
 
     @Override
-    public Class<?>[] acceptChange(Changer.@NotNull ChangeMode mode) {
-        return mode == Changer.ChangeMode.SET ? new Class[]{ItemType.class} : null;
+    protected String getPropertyName() {
+        return "brewing fuel";
     }
 
     @Override
-    public void change(@NotNull Event e, Object @NotNull [] delta, Changer.@NotNull ChangeMode mode) {
-        if (delta[0] instanceof ItemType itemType) {
-            Block block = blockExpression.getSingle(e);
-            if (block != null && block.getState() instanceof BrewingStand brewingStand) {
-                brewingStand.getInventory().setFuel(itemType.getRandom());
+    public Class<? extends ItemStack> getReturnType() {
+        return ItemStack.class;
+    }
+
+    @Override
+    public void change(Event event, @Nullable Object[] delta, Changer.ChangeMode mode) {
+        ItemStack fuel = (mode == Changer.ChangeMode.SET && delta != null && delta[0] instanceof ItemStack item) ? item : ItemStack.empty();
+        getExpr().stream(event).forEach(block -> {
+            if (block.getState() instanceof BrewingStand brewingStand) {
+                BrewerInventory brewerInventory = brewingStand.getInventory();
+                brewerInventory.setFuel(fuel);
                 brewingStand.update();
             }
-        }
+        });
+
     }
 
     @Override
-    public boolean isSingle() {
-        return true;
-    }
-
-    @Override
-    public @NotNull Class<? extends ItemType> getReturnType() {
-        return ItemType.class;
-    }
-
-    @Override
-    public @NotNull String toString(@Nullable Event e, boolean debug) {
-        return "the brewing fuel of " + (e == null ? "" : blockExpression.toString(e, debug));
+    public @Nullable Class<?>[] acceptChange(Changer.ChangeMode mode) {
+        return switch (mode) {
+            case SET -> new Class<?>[] { ItemStack.class };
+            case DELETE, RESET -> new Class<?>[0];
+            default -> null;
+        };
     }
 }
