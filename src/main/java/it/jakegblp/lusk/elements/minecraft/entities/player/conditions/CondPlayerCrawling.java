bfa53f04fe06fb2f51066bea644eecdf790a2f25
Jake <gabrieleluparelloj@gmail.com>
2024-12-06 08:23:00 +0100

    Made crawling condition plural and improved documentation

--- src/main/java/it/jakegblp/lusk/elements/minecraft/entities/player/conditions/CondPlayerCrawling.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/entities/player/conditions/CondPlayerCrawling.java
@@ -8,15 +8,15 @@ import ch.njol.skript.doc.Since;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-@Name("is Crawling")
-@Description("Checks if a player is crawling")
 import static it.jakegblp.lusk.utils.PlayerUtils.isCrawling;
 
+@Name("Player - is Crawling")
+@Description("Checks if a player is crawling.")
 @Examples({"on damage of player:\n\tif victim is crawling:\n\t\tif chance of 0.5:\n\t\t\tcancel event\n\t\t\tsend \"Your attack has failed!\" to attacker"})
 @Since("1.0.0")
 public class CondPlayerCrawling extends PropertyCondition<Player> {
     static {
-        register(CondPlayerCrawling.class, "crawling", "player");
+        register(CondPlayerCrawling.class, "crawling", "players");
     }
 
     @Override
