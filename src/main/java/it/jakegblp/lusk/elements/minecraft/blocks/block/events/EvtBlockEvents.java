da680e5c508c71a5fe348fff0bbba8cfdb78f61c
Jake <gabrieleluparelloj@gmail.com>
2024-12-11 16:30:31 +0100

    Removed unnecessary class exists call

--- src/main/java/it/jakegblp/lusk/elements/minecraft/blocks/block/events/EvtBlockEvents.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/blocks/block/events/EvtBlockEvents.java
@@ -10,18 +10,16 @@ import org.jetbrains.annotations.NotNull;
 
 public class EvtBlockEvents {
     static {
-        if (Skript.classExists("org.bukkit.event.block.FluidLevelChangeEvent")) {
-            Skript.registerEvent("Block - on Fluid Level Change", SimpleEvent.class, FluidLevelChangeEvent.class, "fluid level chang(e[d]|ing)")
-                    .description("""
-                            Called when the fluid level of a block changes due to changes in adjacent blocks.""")
-                    .examples("")
-                    .since("1.0.4");
-            EventValues.registerEventValue(FluidLevelChangeEvent.class, BlockData.class, new Getter<>() {
-                @Override
-                public @NotNull BlockData get(final FluidLevelChangeEvent e) {
-                    return e.getNewData();
-                }
-            }, EventValues.TIME_NOW);
-        }
+        Skript.registerEvent("Block - on Fluid Level Change", SimpleEvent.class, FluidLevelChangeEvent.class, "fluid level chang(e[d]|ing)")
+                .description("""
+                        Called when the fluid level of a block changes due to changes in adjacent blocks.""")
+                .examples("")
+                .since("1.0.4");
+        EventValues.registerEventValue(FluidLevelChangeEvent.class, BlockData.class, new Getter<>() {
+            @Override
+            public @NotNull BlockData get(final FluidLevelChangeEvent e) {
+                return e.getNewData();
+            }
+        }, EventValues.TIME_NOW);
     }
 }
