65ffeb67d12657118b397a867eb767fc1f38559f
Jake <gabrieleluparelloj@gmail.com>
2024-12-11 07:16:16 +0100

    Added fox classinfo

--- src/main/java/it/jakegblp/lusk/elements/minecraft/entities/fox/types/FoxClassInfos.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/entities/fox/types/FoxClassInfos.java
@@ -1,6 +1,9 @@
 package it.jakegblp.lusk.elements.minecraft.entities.fox.types;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.classes.data.DefaultChangers;
+import ch.njol.skript.expressions.base.EventValueExpression;
 import ch.njol.skript.registrations.Classes;
 import it.jakegblp.lusk.api.wrappers.EnumRegistryWrapper;
 import org.bukkit.entity.Fox;
@@ -8,6 +11,15 @@ import org.bukkit.entity.Fox;
 @SuppressWarnings("unused")
 public class FoxClassInfos {
     static {
+        if (Classes.getExactClassInfo(Fox.class) == null) {
+            Classes.registerClass(new ClassInfo<>(Fox.class, "fox")
+                    .user("fox(es)?")
+                    .name("Fox")
+                    .description("A fox.") // add example
+                    .since("1.3")
+                    .defaultExpression(new EventValueExpression<>(Fox.class))
+                    .changer(DefaultChangers.entityChanger));
+        }
         if (Skript.classExists("org.bukkit.entity.Fox$Type") && Classes.getExactClassInfo(Fox.Type.class) == null) {
             Classes.registerClass(
                     new EnumRegistryWrapper(Fox.Type.class, null, "fox_type")
