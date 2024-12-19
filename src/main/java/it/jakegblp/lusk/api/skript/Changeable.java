bdb9d8cbcc5026630fd2072440570f06fcc383ba
Jake <gabrieleluparelloj@gmail.com>
2024-12-19 06:33:46 +0100

    Added Changeable interface for easier api extensions

---
+++ src/main/java/it/jakegblp/lusk/api/skript/Changeable.java
@@ -0,0 +1,87 @@
+package it.jakegblp.lusk.api.skript;
+
+public interface Changeable<F,T> {
+    /**
+     * @return whether this property can be set.
+     * {@inheritDoc}
+     */
+    default boolean allowSet() {
+        return false;
+    }
+    /**
+     * @return whether this property can be reset.
+     * {@inheritDoc}
+     */
+    default boolean allowReset() {
+        return false;
+    }
+    /**
+     * @return whether this property can be added to.
+     * {@inheritDoc}
+     */
+    default boolean allowAdd() {
+        return false;
+    }
+    /**
+     * @return whether this property can be removed from.
+     * {@inheritDoc}
+     */
+    default boolean allowRemove() {
+        return false;
+    }
+    /**
+     * @return whether this property can be mass removed from.
+     * {@inheritDoc}
+     */
+    default boolean allowRemoveAll() {
+        return false;
+    }
+    /**
+     * @return whether this property can be deleted.
+     * {@inheritDoc}
+     */
+    default boolean allowDelete() {
+        return false;
+    }
+
+    /**
+     * Sets this property for {@link F} to {@link T}.
+     * @param from the origin of this property
+     * @param to the new value of this property
+     * {@inheritDoc}
+     */
+    default void set(F from, T to) {}
+    /**
+     * Adds {@link T} to this property for {@link F}.
+     * @param from the origin of this property
+     * @param to the value to add to this property
+     * {@inheritDoc}
+     */
+    default void add(F from, T to) {}
+    /**
+     * Removes {@link T} from this property for {@link F}.
+     * @param from the origin of this property
+     * @param to the value to remove from this property
+     * {@inheritDoc}
+     */
+    default void remove(F from, T to) {}
+    /**
+     * Removes all of {@link T} from this property for {@link F}.
+     * @param from the origin of this property
+     * @param to the value to mass remove from this property
+     * {@inheritDoc}
+     */
+    default void removeAll(F from, T to) {}
+    /**
+     * Deletes this property for {@link F}.
+     * @param from the origin of this property
+     * {@inheritDoc}
+     */
+    default void delete(F from) {}
+    /**
+     * Resets this property for {@link F}.
+     * @param from the origin of this property
+     * {@inheritDoc}
+     */
+    default void reset(F from) {}
+}
