993a33a580b53dde01c23bcd61871b2b25655a93
Jake <gabrieleluparelloj@gmail.com>
2024-12-24 15:54:09 +0100

    Added block damage update event and updated docs for the fluid level change event.

--- src/main/java/it/jakegblp/lusk/elements/minecraft/blocks/block/events/EvtBlockEvents.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/blocks/block/events/EvtBlockEvents.java
@@ -4,16 +4,21 @@ import ch.njol.skript.Skript;
 import ch.njol.skript.lang.util.SimpleEvent;
 import ch.njol.skript.registrations.EventValues;
 import ch.njol.skript.util.Getter;
+import io.papermc.paper.event.block.BlockBreakProgressUpdateEvent;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.block.FluidLevelChangeEvent;
 import org.jetbrains.annotations.NotNull;
 
+import static ch.njol.skript.paperlib.PaperLib.isPaper;
+import static it.jakegblp.lusk.utils.Constants.MINECRAFT_1_20_4;
+
 public class EvtBlockEvents {
     static {
-        Skript.registerEvent("Block - on Fluid Level Change", SimpleEvent.class, FluidLevelChangeEvent.class, "fluid level chang(e[d]|ing)")
+        Skript.registerEvent("on Block Fluid Level Change", SimpleEvent.class, FluidLevelChangeEvent.class, "fluid level chang(e[d]|ing)")
                 .description("""
                         Called when the fluid level of a block changes due to changes in adjacent blocks.""")
-                .examples("")
+                .examples("on fluid level change:")
                 .since("1.0.4");
         EventValues.registerEventValue(FluidLevelChangeEvent.class, BlockData.class, new Getter<>() {
             @Override
@@ -21,5 +26,27 @@ public class EvtBlockEvents {
                 return e.getNewData();
             }
         }, EventValues.TIME_NOW);
+        if (MINECRAFT_1_20_4 && isPaper()) {
+            Skript.registerEvent("on Block Damage Update", SimpleEvent.class, BlockBreakProgressUpdateEvent.class,
+                    "block damag(ing|e) update", "block break progress update")
+                    .description("""
+                            Called when the progress of a block break is updated.
+                            `event-number` = the block damage progress, ranges from 0.0 to 1.0, where 0 is no damage and 1.0 is the most damage.""")
+                    .examples("on block damage update:")
+                    .since("1.3");
+            EventValues.registerEventValue(BlockBreakProgressUpdateEvent.class, Number.class, new Getter<>() {
+                @Override
+                public @NotNull Number get(final BlockBreakProgressUpdateEvent e) {
+                    return e.getProgress();
+                }
+            }, EventValues.TIME_NOW);
+            EventValues.registerEventValue(BlockBreakProgressUpdateEvent.class, Entity.class, new Getter<>() {
+                @Override
+                public @NotNull Entity get(final BlockBreakProgressUpdateEvent e) {
+                    return e.getEntity();
+                }
+            }, EventValues.TIME_NOW);
+
+        }
     }
 }
