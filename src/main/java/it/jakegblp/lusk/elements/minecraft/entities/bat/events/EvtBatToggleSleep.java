a127016c3ce5177f17f4748a362e6658619163d4
Jake <gabrieleluparelloj@gmail.com>
2024-12-27 08:24:03 +0100

    Logic improvements for EvtCreeperIgnite and EvtBatToggleSleep

--- src/main/java/it/jakegblp/lusk/elements/minecraft/entities/bat/events/EvtBatToggleSleep.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/entities/bat/events/EvtBatToggleSleep.java
@@ -4,6 +4,8 @@ import ch.njol.skript.Skript;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+import it.jakegblp.lusk.utils.LuskUtils;
 import org.bukkit.event.Event;
 import org.bukkit.event.entity.BatToggleSleepEvent;
 import org.jetbrains.annotations.NotNull;
@@ -12,37 +14,39 @@ import org.jetbrains.annotations.Nullable;
 @SuppressWarnings("unused")
 public class EvtBatToggleSleep extends SkriptEvent {
     static {
-        Skript.registerEvent("Bat - on Sleep/Wake up", EvtBatToggleSleep.class, BatToggleSleepEvent.class, "bat sleep[ing]", "bat wak(e|ing) up", "bat sleep[ing] toggle[d]")
+        Skript.registerEvent("Bat - on Sleep/Wake up", EvtBatToggleSleep.class, BatToggleSleepEvent.class,
+                        "bat sleep[ing]",
+                        "bat wak(e|ing) up",
+                        "bat sleep[ing] toggle[d]")
                 .description("Called when a bat attempts to sleep or wake up from its slumber.")
                 .examples("on bat wake up:\n\tbroadcast \"A bat has woken up!\"")
                 .since("1.0.0, 1.0.2 (Toggle)");
     }
 
-    private Boolean sleep;
+    private Kleenean sleep;
 
     @Override
     public boolean init(Literal<?> @NotNull [] args, int matchedPattern, @NotNull ParseResult parseResult) {
-        if (matchedPattern == 0) {
-            sleep = true;
-        } else if (matchedPattern == 1) {
-            sleep = false;
-        }
+        sleep = LuskUtils.getKleenean(matchedPattern == 0, matchedPattern == 1);
         return true;
     }
 
     @Override
     public boolean check(@NotNull Event e) {
-        if (sleep == null) return true;
-        if (sleep) {
-            return !((BatToggleSleepEvent) e).isAwake();
-        } else {
-            return ((BatToggleSleepEvent) e).isAwake();
-        }
+        return switch (sleep) {
+            case UNKNOWN -> false;
+            case FALSE -> ((BatToggleSleepEvent) e).isAwake();
+            case TRUE -> !((BatToggleSleepEvent) e).isAwake();
+        };
     }
 
     @Override
     public @NotNull String toString(@Nullable Event e, boolean debug) {
-        return "bat " + (sleep == null ? "sleep toggle" : (sleep ? "sleep" : "wake up"));
+        return "bat " + switch (sleep) {
+            case UNKNOWN -> "sleep toggle";
+            case FALSE -> "wake up";
+            case TRUE -> "sleep";
+        };
     }
 
 }
--- src/main/java/it/jakegblp/lusk/elements/minecraft/entities/creeper/events/EvtCreeperIgnite.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/entities/creeper/events/EvtCreeperIgnite.java
@@ -4,7 +4,9 @@ import ch.njol.skript.Skript;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
 import com.destroystokyo.paper.event.entity.CreeperIgniteEvent;
+import it.jakegblp.lusk.utils.LuskUtils;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -24,30 +26,29 @@ public class EvtCreeperIgnite extends SkriptEvent {
         }
     }
 
-    private Boolean ignite;
+    private Kleenean ignite;
 
     @Override
     public boolean init(Literal @NotNull [] args, int matchedPattern, @NotNull ParseResult parseResult) {
-        if (matchedPattern == 0) {
-            ignite = true;
-        } else if (matchedPattern == 1) {
-            ignite = false;
-        }
+        ignite = LuskUtils.getKleenean(matchedPattern == 0, matchedPattern == 1);
         return true;
     }
 
     @Override
     public boolean check(@NotNull Event e) {
-        if (ignite == null) return true;
-        if (ignite) {
-            return ((CreeperIgniteEvent) e).isIgnited();
-        } else {
-            return !((CreeperIgniteEvent) e).isIgnited();
-        }
+        return switch (ignite) {
+            case UNKNOWN -> false;
+            case FALSE -> !((CreeperIgniteEvent) e).isIgnited();
+            case TRUE -> ((CreeperIgniteEvent) e).isIgnited();
+        };
     }
 
     @Override
     public @NotNull String toString(@Nullable Event e, boolean debug) {
-        return "creeper " + (ignite == null ? "ignite toggle" : (ignite ? "ignite" : "extinguish"));
+        return "creeper "  + switch (ignite) {
+            case UNKNOWN -> "ignite toggle";
+            case FALSE -> "extinguish";
+            case TRUE -> "ignite";
+        };
     }
 }
