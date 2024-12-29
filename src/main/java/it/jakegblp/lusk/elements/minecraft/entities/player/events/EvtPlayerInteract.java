472acf649390f6f88e15b7cd59e40fe89acb1123
Jake <gabrieleluparelloj@gmail.com>
2024-12-29 04:13:03 +0100

    Added more flexibility to EvtPlayerInteract by allowing the user to choose the specific hand, and whether it should be based on a block or an entity, both choices can be omitted.

--- src/main/java/it/jakegblp/lusk/elements/minecraft/entities/player/events/EvtPlayerInteract.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/entities/player/events/EvtPlayerInteract.java
@@ -6,7 +6,9 @@ import ch.njol.skript.lang.SkriptEvent;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.registrations.EventValues;
 import ch.njol.skript.util.Getter;
+import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
+import it.jakegblp.lusk.utils.LuskUtils;
 import org.bukkit.Location;
 import org.bukkit.event.Event;
 import org.bukkit.event.block.Action;
@@ -22,7 +24,7 @@ public class EvtPlayerInteract extends SkriptEvent {
     static {
         Skript.registerEvent("Player - on Interact", EvtPlayerInteract.class,
                 CollectionUtils.array(PlayerInteractEvent.class, PlayerInteractAtEntityEvent.class),
-                        "player interact[ing] [entity:(on|with|at) [an] entity|(with|on) [a] block]")
+                        "player [(:main|:off)[ |-]hand [slot]] interact[ing|ion] [entity:(on|with|at) [an] entity|block:(with|on) [a] block]")
                 .description("""
                         Called when a player interacts with a block or entity by clicking.
                         
@@ -33,7 +35,7 @@ public class EvtPlayerInteract extends SkriptEvent {
                         `event-vector` the vector from the player to the interaction point
                         `event-location` the location of the interaction point
                         """)
-                .examples("on player interact:", "on player interacting with an entity:\n\tif event-equipmentslot = hand slot:")
+                .examples("on player main hand interaction:", "on player interacting with an entity:\n\tif event-equipmentslot = hand slot:")
                 .since("1.3");
 
         EventValues.registerEventValue(PlayerInteractEvent.class, EquipmentSlot.class, new Getter<>() {
@@ -88,21 +90,45 @@ public class EvtPlayerInteract extends SkriptEvent {
         }, EventValues.TIME_NOW);
     }
 
-    private boolean interactAtEntity;
+    private Kleenean isEntityInteraction;
+    private Kleenean isMainHandInteraction;
 
     @Override
     public boolean init(Literal<?>[] args, int matchedPattern, SkriptParser.ParseResult parseResult) {
-        interactAtEntity = parseResult.hasTag("entity");
+        isEntityInteraction = LuskUtils.getKleenean(parseResult.hasTag("entity"), parseResult.hasTag("block"));
+        isMainHandInteraction = LuskUtils.getKleenean(parseResult.hasTag("main"), parseResult.hasTag("off"));
         return true;
     }
 
     @Override
-    public boolean check(Event event) {
-        return (interactAtEntity && event instanceof PlayerInteractEntityEvent) || (!interactAtEntity && event instanceof PlayerInteractEvent);
+    public boolean check(Event e) {
+        boolean result = false;
+        EquipmentSlot hand = null;
+        if (!isEntityInteraction.isFalse() && e instanceof PlayerInteractAtEntityEvent event) {
+            result = true;
+            hand = event.getHand();
+        } else if (!isEntityInteraction.isTrue() && e instanceof PlayerInteractEvent event) {
+            result = true;
+            hand = event.getHand();
+        }
+        if (!result || hand == null) return result;
+        return switch (isMainHandInteraction) {
+            case TRUE -> hand.equals(EquipmentSlot.HAND);
+            case FALSE -> hand.equals(EquipmentSlot.OFF_HAND);
+            case UNKNOWN -> true;
+        };
     }
 
     @Override
     public String toString(@Nullable Event event, boolean debug) {
-        return "player interact with a" + (interactAtEntity ? "n entity" : " block");
+        return "player " + (switch (isMainHandInteraction) {
+            case TRUE -> "main hand ";
+            case FALSE -> "off hand ";
+            case UNKNOWN -> "";
+        }) + "interaction" + switch (isEntityInteraction) {
+            case TRUE -> " with an entity";
+            case FALSE -> " with a block";
+            case UNKNOWN -> "";
+        };
     }
 }
