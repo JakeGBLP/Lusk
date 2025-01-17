39dfcb96b72214e736ac6a6652aec032aa741001
Jake <gabrieleluparelloj@gmail.com>
2025-01-17 03:45:15 +0100

    Potential fix for Checker deprecation - 4.1

--- src/main/java/it/jakegblp/lusk/elements/minecraft/boundingbox/conditions/CondBoundingBoxOverlaps.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/boundingbox/conditions/CondBoundingBoxOverlaps.java
@@ -14,9 +14,7 @@ import org.bukkit.util.BoundingBox;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.function.Predicate;
-
-import static it.jakegblp.lusk.utils.DeprecationUtils.asPredicateOrChecker;
+import static it.jakegblp.lusk.utils.DeprecationUtils.test;
 
 @Name("Bounding Box - Overlaps")
 @Description("Whether or not one or more bounding boxes overlap other bounding boxes.\nBounding boxes that are only intersecting at the borders are not considered overlapping.")
@@ -48,7 +46,7 @@ public class CondBoundingBoxOverlaps extends Condition {
 
     @Override
     public boolean check(@NotNull Event event) {
-        return boundingBox1.check(event, asPredicateOrChecker((Predicate<BoundingBox>) box1 -> boundingBox2.check(event, asPredicateOrChecker((Predicate<BoundingBox>) box1::overlaps))), isNegated());
+        return test(event,boundingBox1, box1 -> test(event, boundingBox2, box1::overlaps), isNegated());
     }
 
     @Override
