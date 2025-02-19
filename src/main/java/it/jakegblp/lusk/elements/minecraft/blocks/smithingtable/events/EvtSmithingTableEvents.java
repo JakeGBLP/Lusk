b1a1c23a8f0d4939d207489364935c336e3c8e6e
Jake <gabrieleluparelloj@gmail.com>
2025-02-19 05:28:22 +0100

    Removed event-inventory as its already present in registered super event

--- src/main/java/it/jakegblp/lusk/elements/minecraft/blocks/smithingtable/events/EvtSmithingTableEvents.java
+++ src/main/java/it/jakegblp/lusk/elements/minecraft/blocks/smithingtable/events/EvtSmithingTableEvents.java
@@ -3,9 +3,6 @@ package it.jakegblp.lusk.elements.minecraft.blocks.smithingtable.events;
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.util.SimpleEvent;
 import org.bukkit.event.inventory.SmithItemEvent;
-import org.bukkit.inventory.Inventory;
-
-import static it.jakegblp.lusk.utils.CompatibilityUtils.registerEventValue;
 
 public class EvtSmithingTableEvents {
     static {
@@ -15,7 +12,6 @@ public class EvtSmithingTableEvents {
                     .description("Called when the recipe of an Item is completed inside a smithing table.")
                     .examples("on item smith:")
                     .since("1.3.4");
-            registerEventValue(SmithItemEvent.class, Inventory.class, SmithItemEvent::getInventory, 0);
         }
     }
 }
