ce4622d8083cafeed24535b5ef8271f749d2d2be
BuildTools <unconfigured@null.spigotmc.org>
2025-12-16 14:28:16 +0000

    Fixed packet listener

---
+++ nms-core/src/main/java/it/jakegblp/lusk/nms/core/protocol/packets/client/TeamPacket.java
@@ -0,0 +1,8 @@
+package it.jakegblp.lusk.nms.core.protocol.packets.client;
+
+public class TeamPacket implements ClientboundPacket{
+    @Override
+    public Object asNMS() {
+        return null;
+    }
+}
---
+++ nms-core/src/main/java/it/jakegblp/lusk/nms/core/world/player/TeamData.java
@@ -0,0 +1,16 @@
+package it.jakegblp.lusk.nms.core.world.player;
+
+import static it.jakegblp.lusk.nms.core.AbstractNMS.NMS;
+
+public class TeamData {
+
+    public Object packet;
+
+    String teamName;
+
+
+    public Object build(){
+        return NMS
+    }
+
+}
