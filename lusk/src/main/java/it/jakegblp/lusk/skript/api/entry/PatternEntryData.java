3cc949ff1322f0830318c890a38bc751ff8b292a
Jake <gabrieleluparelloj@gmail.com>
2026-02-02 01:04:18 +0100

    Initial pattern entry data version

---
+++ lusk/src/main/java/it/jakegblp/lusk/skript/api/entry/PatternEntryData.java
@@ -0,0 +1,33 @@
+package it.jakegblp.lusk.skript.api.entry;
+
+import ch.njol.skript.ScriptLoader;
+import ch.njol.skript.config.Node;
+import ch.njol.skript.config.SimpleNode;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.patterns.PatternCompiler;
+import ch.njol.skript.patterns.SkriptPattern;
+import lombok.Getter;
+import org.jetbrains.annotations.Nullable;
+import org.skriptlang.skript.lang.entry.util.ExpressionEntryData;
+
+@Getter
+public class PatternEntryData<T> extends ExpressionEntryData<T> {
+
+    public final SkriptPattern pattern;
+
+    public PatternEntryData(String key, String pattern, @Nullable Expression<? extends T> defaultValue, boolean optional, Class<? extends T> returnType) {
+        super(key, defaultValue, optional, returnType);
+        this.pattern = PatternCompiler.compile(pattern);
+    }
+
+    @Override
+    public boolean canCreateWith(Node node) {
+        if (!(node instanceof SimpleNode))
+            return false;
+        String key = node.getKey();
+        if (key == null)
+            return false;
+        key = ScriptLoader.replaceOptions(key);
+        return pattern.match(key) != null;
+    }
+}
